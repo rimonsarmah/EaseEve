f1cd53aa40c45e232ec0010266c0012c469ac1cc
Rimon Sarmah <rimonsarmah@Rimons-MacBook-Pro.local>
2021-12-12 15:02:39 +0530

    Start

---
+++ src/main/java/com/easeeve/controllers/TestController.java
@@ -0,0 +1,12 @@
+package com.easeeve.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping( "test")
+    public String testGet(){
+        return "testSuccess";
+    }
+}
